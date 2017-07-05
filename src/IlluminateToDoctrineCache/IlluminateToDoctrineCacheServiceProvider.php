a9fefb6e8c1660db041382141fc17b2bd17a96c4
Iain Connor <Iain.Connor@priceline.com>
2017-07-05 13:31:43 -0500

    Welcome aboard.

---
+++ src/IlluminateToDoctrineCache/IlluminateToDoctrineCacheServiceProvider.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace IainConnor\IlluminateToDoctrineCache;
+
+use Illuminate\Support\ServiceProvider;
+
+class IlluminateToDoctrineCacheServiceProvider extends ServiceProvider
+{
+
+}
